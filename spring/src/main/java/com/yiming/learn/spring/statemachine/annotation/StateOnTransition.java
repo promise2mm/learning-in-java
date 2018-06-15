284a740c91a4e9a2dcfd6edc1d9449569ecfe2db
一鸣 <nishibin@cai-inc.com>
2018-06-15 10:25:23 +0800

    spring-state-machine

---
+++ spring/src/main/java/com/yiming/learn/spring/statemachine/annotation/StateOnTransition.java
@@ -0,0 +1,15 @@
+package com.yiming.learn.spring.statemachine.annotation;
+
+import java.lang.annotation.Retention;
+import org.springframework.statemachine.annotation.OnTransition;
+
+/**
+ * Created by yiming on 2018-06-08 11:39. Description:
+ *
+ * @author yiming
+ */
+//@Retention()
+@OnTransition
+public @interface StateOnTransition {
+
+}
