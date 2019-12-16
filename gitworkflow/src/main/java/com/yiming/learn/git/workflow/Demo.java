4453dea16aba266a4adb221a87c59abb522e0ea6
一鸣 <nishibin@cai-inc.com>
2019-12-16 20:27:41 +0800

    revert done

--- gitworkflow/src/main/java/com/yiming/learn/git/workflow/Demo.java
+++ gitworkflow/src/main/java/com/yiming/learn/git/workflow/Demo.java
@@ -17,6 +17,6 @@ public class Demo {
 
     public void testRevert() {
         System.out.println("base commit.");
-        System.out.println("do not commit.");
+        System.out.println("commit won't be push.");
     }
 }
